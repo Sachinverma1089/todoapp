1713f85b175d80a31b5c51480912d0b4ed4dba2f
Sachinverma1089 <87863276+Sachinverma1089@users.noreply.github.com>
2023-07-08 19:23:09 +0530

    Add files via upload

---
+++ Todoapp/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:todoapp/screens/home.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  static const String _title = 'Flutter Stateful Clicker Counter';
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "ToDo App",
+      home: Home(),
+    );
+  }
+}
